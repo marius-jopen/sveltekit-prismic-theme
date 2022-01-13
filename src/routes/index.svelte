9fe888e7eb56a370be7a2c7c695385da0d57938b
mariusjopen_thepeople <marius.jopen@thepeople.world>
2022-01-13 21:05:18 +0100

    paragraph module done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,9 +17,12 @@
 <script>
 	import HeadlineSimple from '$lib/addons/slices/headlines/headlineSimple.svelte'
 	import ImageSimple from "$lib/addons/slices/images/imageSimple.svelte"
+	import ParagraphRichtext from "$lib/addons/slices/paragraphs/paragraphRichtext.svelte"
 
     export let document
+	console.log(document)
 </script>
 
 <HeadlineSimple inputHeadline={document.data.title[0].text} />
 <ImageSimple inputImage={document.data.image} />
+<ParagraphRichtext inputParagraph={document.data.long_text} />
