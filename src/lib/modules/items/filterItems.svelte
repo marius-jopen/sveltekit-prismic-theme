586b1f3733c82508064062f613f77137c062500b
mariusjopen_thepeople <marius.jopen@thepeople.world>
2022-01-14 20:13:43 +0100

    filter mobile

--- src/lib/modules/items/filterItems.svelte
+++ src/lib/modules/items/filterItems.svelte
@@ -15,14 +15,14 @@
 	})
 </script>
 
-<div class="overflow-scroll sm:overflow-auto width-full border-b border-lines bg-white">
-	<div class="box sm:justify-center flex flex-row sm:w-full py-3 sm:px-4">
+<div class="overflow-scroll sm:overflow-auto width-full border-b border-lines bg-white sm:flex sm:justify-center">
+	<div class="box sm:justify-center flex flex-row sm:w-full py-3 sm:px-4 mr-6 sm:mr-0">
 		<a class="sm:text-center py-1 sm:py-0 ml-6 text-lg textHoverGrey" href="/{type}s" sveltekit:prefetch >
 			All
 		</a>
 
 		{#each filteredItems as filterItem}
-			<a class="sm:text-center py-1 sm:py-0 ml-6 last:ml-0 text-lg textHoverGrey" href="/{type}s/category/{filterItem.replace(" ", '-').toLowerCase()}" sveltekit:prefetch >
+			<a class="sm:text-center py-1 sm:py-0 ml-6 text-lg textHoverGrey" href="/{type}s/category/{filterItem.replace(" ", '-').toLowerCase()}" sveltekit:prefetch >
 				{filterItem}
 			</a>
 		{/each}
@@ -33,7 +33,8 @@
 <style lang="postcss">
 	@media screen and (max-width: 640px) {
 		.box {
-			width: 150%;
+			display: -webkit-box;
+    		width: max-content;
 		}
 	}
 </style>
