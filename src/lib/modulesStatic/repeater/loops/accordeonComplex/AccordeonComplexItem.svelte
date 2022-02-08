a78185ac2c51c912f68be31f2f99b4e5ee40e289
Marius Jopen <mail@mariusjopen.com>
2022-02-08 17:09:25 +0100

    animated the list

--- src/lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexItem.svelte
+++ src/lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexItem.svelte
@@ -31,28 +31,32 @@
 	}
 </script>
 
-<div on:click={toggle} class="textHoverGrey cursor-pointer flex text-xl py-3 px-4 w-full border-b border-lines">
-	<div class="w-1/3">
-		{item.data.title[0].text}
-	</div>
+{#if item}
+	<div transition:slide={{ duration: 300 }}>
+		<div on:click={toggle} class="textHoverGrey cursor-pointer flex text-xl py-3 px-4 w-full border-b border-lines">
+			<div class="w-1/3">
+				{item.data.title[0].text}
+			</div>
 
-	{#if item.data.sub_title[0]}
-		<div class="w-1/3">
-			{item.data.sub_title[0].text}
-		</div>
-	{/if}
+			{#if item.data.sub_title[0]}
+				<div class="w-1/3">
+					{item.data.sub_title[0].text}
+				</div>
+			{/if}
 
-	<div class="w-1/3">
-		{item.last_publication_date}
-	</div>
-</div>
-
-{#if interalStatus == true}
-	<div class="bg-neutral-100 flex w-full border-b border-lines py-4 px-4" transition:slide={{ duration: 300 }}>
-		<div class="w-1/2">
-			{item.data.title[0].text}
+			<div class="w-1/3">
+				{item.last_publication_date}
+			</div>
 		</div>
 
-		<img src="{item.data.thumbnail.Big.url}" class="w-1/2 h-30vw object-cover" alt="">
+		{#if interalStatus == true}
+			<div class="bg-neutral-100 flex w-full border-b border-lines py-4 px-4" transition:slide={{ duration: 300 }}>
+				<div class="w-1/2">
+					{item.data.title[0].text}
+				</div>
+
+				<img src="{item.data.thumbnail.Big.url}" class="w-1/2 h-30vw object-cover" alt="">
+			</div>
+		{/if}
 	</div>
 {/if}
