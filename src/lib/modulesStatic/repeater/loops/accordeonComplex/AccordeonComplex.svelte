bf5b52c887e7422a86016009ced29be9153b9555
Marius Jopen <mail@mariusjopen.com>
2022-02-11 16:57:09 +0100

    accordeon scroll and opens the one with the right # in the url

--- src/lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplex.svelte
+++ src/lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplex.svelte
@@ -1,4 +1,5 @@
 <script>
+	import AccordeonComplexUrl from '$lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexUrl.svelte'
 	import AccordeonComplexSearch from '$lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexSearch.svelte'
 	import AccordeonComplexSort from '$lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexSort.svelte'
 	import AccordeonComplexLoop from '$lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexLoop.svelte'
@@ -10,6 +11,7 @@
 	// Get values from the components with binding
 	export let itemsSorted
 	export let itemsSearched
+	export let itemsUrl
 
 	// Declate 2 variables which we need
 	let itemsModified
@@ -24,6 +26,7 @@
 	// make searched and sorted variables reative
 	$: itemsSearched, setSearched() 
 	$: itemsSorted, setSorted() 
+	$: itemsUrl, setUrl()
 
 	// Function which gets called on each keystroke of the search
 	// It filters the list with the items and also the list which the sort function is using
@@ -40,15 +43,17 @@
 			itemsModified = itemsSorted
 		}
 	}
+
+	function setUrl() {
+		if (itemsUrl) {
+			itemsModified = itemsUrl
+		}
+	}
 </script>
 
 <div>
+	<AccordeonComplexUrl items={items} bind:itemsUrl={itemsUrl} />
 	<AccordeonComplexSearch items={items} bind:itemsSearched={itemsSearched}/>
 	<AccordeonComplexSort items={itemsSortedModified} bind:itemsSorted={itemsSorted}/>
 	<AccordeonComplexLoop type={type} items={itemsModified} />
 </div>
-
-<!-- 
-	TODO:
-	URL connection
- -->
---
+++ src/lib/modulesStatic/repeater/loops/accordeonComplex/AccordeonComplexUrl.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { browser } from '$app/env'
+    import * as animateScroll from "svelte-scrollto"
+
+    export let items
+    
+    // Variable for binding
+    export let itemsUrl
+
+    // Init the url variable
+    let urlHash = ""
+
+    // Get url hash from browser
+    $: if(browser) {
+        urlHash = location.hash.replace('#', '')
+    }
+
+    // If URL changes, then call the search function
+    $: urlHash, openItem(urlHash)
+
+    // Make the result of the search function reactive
+    $: itemsUrl = items
+
+    // Function which sets the status of the selected item to true and the others to false
+    function openItem(urlHash) {
+        items.map(item => {
+            if(item.uid == urlHash) {
+                item.status = true
+            } else {
+                item.status = false
+            }
+        })
+
+        // And also scroll to that div with an offset
+        if(browser) {
+            animateScroll.scrollTo({ element: "#bitterfly", offset: -50 })
+        }
+    }
+</script>
