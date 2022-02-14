82efc77cfee0a11c7731f3408f0b47426e51f439
Marius Jopen <mail@mariusjopen.com>
2022-02-14 19:16:54 +0100

    Merge branch 'dev' of https://github.com/marius-jopen/sveltekit-prismic-theme into dev

--- src/utils/client.js
+++
@@ -1,8 +0,0 @@
-import Prismic from '@prismicio/client';
-
-let url = 'mj-theme' // Change this to your Prismic ID
-
-const apiEndpoint = 'https://' + url + '.cdn.prismic.io/api/v2';
-const Client = Prismic.client(apiEndpoint);
-
-export default Client;
