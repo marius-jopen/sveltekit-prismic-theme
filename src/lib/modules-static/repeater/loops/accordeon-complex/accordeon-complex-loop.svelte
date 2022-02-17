30a31700b4dff1bc2cb336122f56276e337688c6
Marius Jopen <mail@mariusjopen.com>
2022-02-17 11:09:39 +0100

    added load more button to accordeon

--- src/lib/modules-static/repeater/loops/accordeon-complex/accordeon-complex-loop.svelte
+++ src/lib/modules-static/repeater/loops/accordeon-complex/accordeon-complex-loop.svelte
@@ -12,9 +12,16 @@
     // Declare a varibale which gets changes when the active accordeon opens
     // It tells us the name of the accordeonm so we can close the others
     let activeItem
+    let visibleItems = 2
+    let loadMoreVisible = true
 
     // Everytime an accordeons opens, close all the others
     $: activeItem, closeInactiveAccordeons(activeItem)
+
+    function loadMore() {
+        visibleItems = 10000
+        loadMoreVisible = false
+    }
 
     // Close all Accordeons which are not active
     function closeInactiveAccordeons(activeItem) {
@@ -33,9 +40,17 @@
     }
 </script>
 
-{#each items as item}
-    <ItemAccordeonList item={item} type={type} bind:interalStatus={item.status} bind:uid={activeItem}/>
+{#each items as item, i}
+    {#if i <= visibleItems}
+        <ItemAccordeonList item={item} type={type} bind:interalStatus={item.status} bind:uid={activeItem}/>
+    {/if}
 {/each}
+
+{#if loadMoreVisible}
+    <div class="text-center text-xl py-3 uppercase cursor-pointer" on:click={loadMore}>
+        Load More
+    </div>
+{/if}
 
 <div class="border-lines border-b w-full -mt-1px">
 </div>
