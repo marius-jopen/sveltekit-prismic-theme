889cea0a20bda36950b60c0f260ffe72c11ce3b4
Marius Jopen <mail@mariusjopen.com>
2022-03-03 22:22:45 +0100

    made layout slide up module

--- src/lib/modules-flex/sliders/slider-video-full-info/slider-video-full-info-item.svelte
+++ src/lib/modules-flex/sliders/slider-video-full-info/slider-video-full-info-item.svelte
@@ -1,7 +1,7 @@
 <script>
     // Import child components which are used in this slider
     import VideoControls from '$lib/modules-flex/videos/video-controls/video-controls.svelte'
-	import InfoBoxes from '$lib/modules-flex/paragraphs/info-boxes/info-boxes.svelte'
+	import LayoutSlideUp from '$lib/modules-static/layouts/layout-slide-up/layout-slide-up.svelte'
 
     // Get data from parent Component
     export let item
@@ -16,39 +16,7 @@
     if(videoVimeo) {
         videoUrl = item.slider_video_link[0].text
     }
-
-    // Variables which are used for the functionality of this comp
-	let y = 0
-	let lastY = 0
-	let infoHeight
-	let marginTop = 0
-
-	// Make variables reactive
-	$: y, updateScroll()
-	$: infoHeight
-
-
-	// Function which detects the scroll direction
-	function updateScroll() {
-		let dy = lastY - y
-
-		// Make sure that the content is visible when the page gets loaded
-		if (y < 1) {
-			marginTop = 0
-		}
-		
-		lastY = y
-
-		//  Checks if scrolling up or down
-		if (dy < 0) {
-			marginTop = infoHeight
-		} else {
-			marginTop = 0
-		}
-	}
 </script>
-
-<svelte:window bind:scrollY={y} />
 
 <div class="{height} fixed top-0 left-0 z-0 w-full">
     {#if item.slider_image.url}
@@ -58,14 +26,4 @@
     <VideoControls index="{index}" status="{status}" inputVideoUrl="{videoUrl}" inputVideoPoster="{item.slider_video_poster.Big.url}" height="h-screen"/>
 </div>
 
-<div class="{height} pointer-event-none"></div>
-
-<div bind:clientHeight={infoHeight} style="margin-top: -{marginTop}px;" class="infoBox relative z-20 bg-white bottom-0 left-0 w-full">
-	<InfoBoxes item={item} />
-</div>
-
-<style lang="postcss">
-	.infoBox {
-		transition: margin-top 0.3s;
-	}
-</style>
+<LayoutSlideUp item={item} />
---
+++ src/lib/modules-static/layouts/layout-slide-up/layout-slide-up.svelte
@@ -0,0 +1,49 @@
+<script>
+    // Import child components which are used in this slider
+	import InfoBoxes from '$lib/modules-flex/paragraphs/info-boxes/info-boxes.svelte'
+
+    // Get data from parent Component
+    export let item
+
+    // Variables which are used for the functionality of this comp
+	let y = 0
+	let lastY = 0
+	let infoHeight
+	let marginTop = 0
+
+	// Make variables reactive
+	$: y, updateScroll()
+
+	// Function which detects the scroll direction
+	function updateScroll() {
+		let dy = lastY - y
+
+		// Make sure that the content is visible when the page gets loaded
+		if (y < 1) {
+			marginTop = 0
+		}
+		
+		lastY = y
+
+		//  Checks if scrolling up or down
+		if (dy < 0) {
+			marginTop = infoHeight
+		} else {
+			marginTop = 0
+		}
+	}
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<div class="h-screen pointer-event-none"></div>
+
+<div bind:clientHeight={infoHeight} style="margin-top: -{marginTop}px;" class="infoBox relative z-20 bg-white bottom-0 left-0 w-full">
+	<InfoBoxes item={item} />
+</div>
+
+<style lang="postcss">
+	.infoBox {
+		transition: margin-top 0.3s;
+	}
+</style>
