2f4e1edac66ff98ac711aa5d717785f04fce415a
Marius Jopen <marius.jopen@thepeople.world>
2022-11-03 20:42:53 +0000

    slicer clean up

--- src/lib/prismic/slices.svelte
+++ src/lib/prismic/slices.svelte
@@ -1,12 +1,7 @@
 <script>
 	import Slice from '$lib/prismic/slice.svelte'
-  import { page } from "$app/stores"
 
 	export let slices
-
-  const props = {
-    // image: $page.url.pathname.includes('home')
-  }
 </script>
 
 {#if slices}
