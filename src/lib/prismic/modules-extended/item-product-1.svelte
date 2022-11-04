9807e151a9ddccfb385fd820232b170c55670554
Marius Jopen <marius.jopen@thepeople.world>
2022-11-04 18:46:13 +0000

    shop extended done

--- src/lib/prismic/modules-extended/item-product-1.svelte
+++ src/lib/prismic/modules-extended/item-product-1.svelte
@@ -2,7 +2,8 @@
   import Link from "$lib/prismic/atoms/link.svelte"
   import Text from "$lib/prismic/atoms/text.svelte"
 	import Image from "$lib/prismic/atoms/image.svelte"
-  import Container from "$lib/tools/container.svelte";
+  import Container from "$lib/tools/container.svelte"
+	import Price from "$lib/prismic/modules-basic/price.svelte"
 
 	export let item
 </script>
@@ -10,7 +11,21 @@
 <Container classes="sm:w-1/2 lg:w-1/3 border-b border-lines-1 text-white border-l" pt='pt-0' pb='pb-0' width='screen'>
 	<Link document={item}>
 		<div class="relative">
-				<Text plain field={item.data.title} classes="absolute top-3 left-4 sm:right-4 z-20 text-right uppercase tracking-widest text-xs pointer-events-none" />
+				<Text plain field={item.data.title} classes="absolute top-3 right-4 z-20 uppercase tracking-widest text-xs pointer-events-none" />
+
+				<div class="absolute bottom-3 right-4 z-20 uppercase tracking-widest text-xs pointer-events-none">
+					<Price {item} />
+				</div>
+
+				<div class="absolute bottom-3 left-4 z-20 uppercase tracking-widest text-xs pointer-events-none">
+					{#if item.data.few_pieces_left}
+						Few Pieces Left
+					{/if}
+
+					{#if item.data.on_sale}
+						On Sale
+					{/if}
+				</div>
 
 				<Image src={item.data.thumbnail} classes='h-[130vw] sm:h-[70vw] lg:h-[37vw] object-cover' />
 
--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -1,9 +1,10 @@
 <script>
 	import Seo from "$lib/prismic/seo.svelte"
   import Categories from "$lib/prismic/modules-basic/categories.svelte"
-  import Products from '$lib/prismic/modules-basic/items-product.svelte'
   import Text from "$lib/prismic/atoms/text.svelte"
 	import Container from "$lib/tools/container.svelte"
+  import ItemsProduct from '$lib/prismic/modules-basic/items-product.svelte'
+	import ItemsProduct1 from '$lib/prismic/modules-extended/items-product-1.svelte'
 
 	export let data
 
@@ -28,9 +29,10 @@
 
 <Seo {setup} {document} />
 
-<Container pt='pt-16'>
+<!-- <Container pt='pt-16'>
 	<Text classes="h4 text-center" plain field={document.data.title} />
-</Container>
+</Container> -->
 
-<Categories type='shop' {category} {categories} />
-<Products items={filtered} />
+<!-- <Categories type='shop' {category} {categories} /> -->
+<!-- <ItemsProduct items={filtered} /> -->
+<ItemsProduct1 items={filtered} />
