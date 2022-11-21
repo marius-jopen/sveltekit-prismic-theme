c31d74f18f3688754ca3a4442c2f1d7ee9d98145
Marius Jopen <marius.jopen@thepeople.world>
2022-11-21 17:25:00 +0700

    2 level navigation done!

--- src/lib/prismic/modules-extended/navigation-2.svelte
+++ src/lib/prismic/modules-extended/navigation-2.svelte
@@ -1,14 +1,76 @@
 <script>
 	import Link from "$lib/prismic/atoms/link.svelte"
 	import Text from "$lib/prismic/atoms/text.svelte"
+	import LayoutAccordeon1 from "$lib/tools/layout-accordeon-1.svelte"
 
 	export let document
+
+	const documentIndexed = []
+	const navigation = []
+	const children = []
+	let parentCount = 0
+	let object
+
+	document.forEach(function (item) {
+		if(item.status == 'normal') {
+			documentIndexed.push(item);
+		}
+
+		if(item.status == 'parent') {
+			documentIndexed.push(item)
+			item.parent = parentCount
+			parentCount = parentCount + 1
+		}
+
+		if(item.status == 'child') {
+			documentIndexed.push(item)
+			item.child = parentCount - 1
+		}
+	})
+
+	documentIndexed.forEach(function (item) {
+		if(item.status == 'normal') {
+			navigation.push(item);
+		}
+
+		if(item.status == 'parent') {
+			navigation.push(item)
+			item.children = []
+		}
+	})
+
+	documentIndexed.forEach(function (item) {
+		if(item.status == 'child') {
+			children.push(item)
+		}
+	})
+
+	children.forEach(function (item) {
+		for (let step = 0; step < parentCount; step++) {
+			if(item.child == step) {
+				object = navigation.find(o => o.parent === step)
+				object.children.push(item)
+			}
+		}
+	})
 </script>
 
 <div class="flex flex-col">
-	{#each document as { title, link }}
-		<Link document={link}>
-			<Text inline field={title} />
-		</Link>
+	{#each navigation as item}
+		{#if item.status == 'normal'}
+			<Link document={item.link}>
+				<Text inline field={item.title} />
+			</Link>
+		{/if}
+
+		{#if item.status == 'parent'}
+			<LayoutAccordeon1 inputHeadline={item.title} >
+				{#each item.children as i}
+					<Link document={i.link}>
+						<Text inline field={i.title} />
+					</Link>
+				{/each}
+			</LayoutAccordeon1>
+		{/if}
 	{/each}
 </div>
---
+++ src/lib/tools/layout-accordeon-1.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { slide } from "svelte/transition"
+	import Text from "$lib/prismic/atoms/text.svelte"
+
+	export let inputHeadline
+
+	let accordeonState = false
+
+	function toggleAccordeon() {
+		if(accordeonState == false) {
+			accordeonState = true
+		} else {
+			accordeonState = false
+		}
+	}
+</script>
+
+<div class="-mt-px">
+	<div on:click={toggleAccordeon} class="cursor-pointer">
+		<Text inline field={inputHeadline} />
+	</div>
+
+	{#if accordeonState == true}
+		<div class="content" transition:slide={{ duration: 300 }}>
+			<div class="flex flex-col">
+				<slot>
+				</slot>
+			</div>
+		</div>
+	{/if}
+</div>
